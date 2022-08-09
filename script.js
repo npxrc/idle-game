9e0c4a23d773d2c587c547b137f55452c08b6b52
soph1428 <78243082+soph1428@users.noreply.github.com>
2022-08-09 15:42:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ if (localStorage.getItem(`clicks`)) clicks = Number(localStorage.getItem(`clicks
 if (localStorage.getItem(`multiplier`)) multiplier = Number(localStorage.getItem(`multiplier`))
 function clickup() {
     clicks += multiplier, updateText(), canClick = false
-    setTimeout(() => {canClick = true}, 250)
+    setTimeout(() => {canClick = true}, 100)
 } function upgrade(clickAmount, cost) {
   if (multiplier >= clickAmount) return alert(`You already bought this.`)
   if (clicks >= cost) {
