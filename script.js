a733cf0e763ea3bb1e36f683e8db24dfd902323d
soph1428 <78243082+soph1428@users.noreply.github.com>
2022-08-09 15:07:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function clickup() {
   clicks += multiplier, updateText()
 } function upgrade(clickAmount, cost) {
   if (clicks >= cost) {
-    if (multiplier != clickAmount) return alert(`You already bought this.`)
+    if (multiplier == clickAmount) return alert(`You already bought this.`)
     clicks -= cost, updateText()
     multiplier = clickAmount, localStorage.setItem(`multiplier`, multiplier)
   } else alert(`You can't buy this.`)
