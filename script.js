344b857505453da7231cc0bca7041b3702d74edc
soph1428 <78243082+soph1428@users.noreply.github.com>
2022-08-09 15:30:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let clicks = 0, multiplier = 1, canClick = true
 if (localStorage.getItem(`clicks`)) clicks = Number(localStorage.getItem(`clicks`))
 if (localStorage.getItem(`multiplier`)) multiplier = Number(localStorage.getItem(`multiplier`))
-function clickup() {li
+function clickup() {
   console.log(`clicked`)
   clicks += multiplier, updateText(), canClick = false
   setTimeout(() => {canClick = true}, 50)
