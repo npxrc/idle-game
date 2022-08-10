1740f795d604e35052425c9d22526b76ecf4449d
soph1428 <78243082+soph1428@users.noreply.github.com>
2022-08-10 09:23:03 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require(`express`)
+const app = express()
+const http = require(`http`)
+const server = http.createServer(app)
+const {Server} = require(`socket.io`)
+const io = new Server(server)
+app.get(`/`, (req, res) => {
+  res.sendFile(__dirname + `/index.html`)
+})
+app.use(express.static(__dirname))
+io.on(`connection`, socket => {
+  console.log(`a user has connected`)
+  socket.on(`upgraded`, (clicks, multiplier) => {
+    console.log(`a user has upgraded and now has a multiplier of ${multiplier} and ${clicks} clicks`)
+  })
+})
